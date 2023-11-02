118cb3d5136b91836eaee6d73946ac0348e83935
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-11-01 21:44:31 -0400

    .

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,6 +8,9 @@ public partial class Player : CharacterBody3D
     [Export]
     public int FallAcceleration { get; set; } = 75;
 
+    [Export]
+    public int JumpImpulse { get; set; } = 20;
+
     private Vector3 _velocity = Vector3.Zero;
 
     public override void _PhysicsProcess(double delta)
@@ -39,10 +42,16 @@ public partial class Player : CharacterBody3D
 
         _velocity.X = direction.X * Speed;
         _velocity.Z = direction.Z * Speed;
+        _velocity.Y = direction.Y;
 
         if (!IsOnFloor())
         {
             _velocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+        {
+            _velocity.Y = JumpImpulse;
         }
 
         Velocity = _velocity;
