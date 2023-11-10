7efeedf3504a352a08f6861d2700d73e83961ede
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-11-09 23:35:03 -0500

    .

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,168 +5,161 @@ using System.Diagnostics;
 // useful: https://stackoverflow.com/questions/73161622/how-to-make-godot-character-jump
 public partial class Player : CharacterBody3D
 {
-	#region Fields
-	private float _halfJumpTime = 0.5f;
+    #region Fields
 
-	private Vector3 _velocity = Vector3.Zero;
+    private float _halfJumpTime = 0.5f;
+    private Vector3 _velocity = Vector3.Zero;
+    private bool canJump = true;
+    private TimeSpan coyoteTime = new TimeSpan(0, 0, 0, 0, 300); 
+    private Stopwatch coyoteTimer = new Stopwatch(); // Coyote time duration in seconds
 
-	private bool canJump = true;
+    #endregion Fields
 
-	private TimeSpan coyoteTime = new TimeSpan(0, 0, 0, 0, 500);
+    #region Properties
 
-	// Coyote time duration in seconds
-	private Stopwatch coyoteTimer = new Stopwatch();
+    private float Gravity
+    {
+        get
+        {
+            return -InitialSpeed / _halfJumpTime;
+        }
+    }
 
-	#endregion Fields
+    private float InitialSpeed
+    {
+        get
+        {
+            return 2.0f * MaxHeight / _halfJumpTime;
+        }
+    }
 
-	#region Properties
+    private float MaxHeight
+    {
+        get
+        {
+            return JumpForce;
+        }
+    }
 
-	private float Gravity
-	{
-		get
-		{
-			return -InitialSpeed / _halfJumpTime;
-		}
-	}
+    [Export]
+    public float JumpForce { get; set; } = 5;
 
-	private float InitialSpeed
-	{
-		get
-		{
-			return 2.0f * MaxHeight / _halfJumpTime;
-		}
-	}
+    [Export]
+    public int Speed { get; set; } = 14;
 
-	private float MaxHeight
-	{
-		get
-		{
-			return JumpHeight;
-		}
-	}
+    #endregion Properties
 
-	[Export]
-	public float JumpHeight { get; set; } = 5;
+    #region Methods
 
-	[Export]
-	public int JumpImpulse { get; set; } = 20;
+    private void ApplyGravity(double delta)
+    {
+        if (!IsOnFloor())
+            _velocity.Y += Gravity * (float)delta;
+        else if (_velocity.Y <= 0)
+            _velocity.Y = 0;
+    }
 
-	[Export]
-	public int Speed { get; set; } = 14;
+    private void HandleInput(double delta)
+    {
+        var direction = Vector3.Zero;
 
-	#endregion Properties
+        if (Input.IsActionPressed("move_up"))
+        {
+            direction.Z -= 1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            direction.Z += 1;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            direction.X -= 1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            direction.X += 1;
+        }
 
-	#region Methods
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            GetNode<Node3D>("Pivot").LookAt(Position - direction, Vector3.Up);
+        }
+        //GD.Print($@"Can: {canJump} + Coyote: {coyoteTimer.Elapsed} + {coyoteTimer.Elapsed != TimeSpan.Zero} + {coyoteTimer.Elapsed < coyoteTime}");
+        // Implement Coyote Time
+        if (IsOnFloor())
+        {
+            coyoteTimer.Reset();
+            canJump = true;
+            //GD.Print($@"Coyote Started");
+        }
+        else if (canJump && coyoteTimer.Elapsed == TimeSpan.Zero)
+        {
+            coyoteTimer.Start();
+            canJump = true;
+            //GD.Print($@"Coyote Jump");
+            //GD.Print($@"Coyote: {coyoteTimer.Elapsed}");
+        }
+        else if (canJump && coyoteTimer.Elapsed != TimeSpan.Zero && coyoteTimer.Elapsed <= coyoteTime)
+        {
+            // do nothing
+        } 
+        else
+        {
+            coyoteTimer.Reset();
+            canJump = false;
+            //GD.Print($@"Coyote Ended");
+        }
+        //GD.Print($@"Coyote: {coyoteTimer.Elapsed}");
+        // Implement Jump Buffering
+        if (Input.IsActionJustPressed("jump"))
+        {
+            Jump();
+        }
 
-	private void ApplyGravity(double delta)
-	{
-		if (!IsOnFloor())
-			_velocity.Y += Gravity * (float)delta;
-		else if (_velocity.Y <= 0)
-			_velocity.Y = 0;
-	}
+        MoveTowards(direction);
+    }
 
-	private void HandleInput(double delta)
-	{
-		var direction = Vector3.Zero;
+    private void MoveTowards(Vector3 direction)
+    {
+        float targetSpeed = direction.Length() > 0 ? Speed : 0f;
+        _velocity.X = Mathf.Lerp(_velocity.X, direction.X * targetSpeed, 0.1f);
+        _velocity.Z = Mathf.Lerp(_velocity.Z, direction.Z * targetSpeed, 0.1f);
+    }
 
-		if (Input.IsActionPressed("move_up"))
-		{
-			direction.Z -= 1;
-		}
-		if (Input.IsActionPressed("move_down"))
-		{
-			direction.Z += 1;
-		}
-		if (Input.IsActionPressed("move_left"))
-		{
-			direction.X -= 1;
-		}
-		if (Input.IsActionPressed("move_right"))
-		{
-			direction.X += 1;
-		}
+    private void Jump()
+    {
+        if (canJump)
+        {
+            _velocity.Y = JumpForce;
+            canJump = false;
+            coyoteTimer.Reset();
+        }
+    }
 
-		if (direction != Vector3.Zero)
-		{
-			direction = direction.Normalized();
-			GetNode<Node3D>("Pivot").LookAt(Position - direction, Vector3.Up);
-		}
-
-		// Implement Coyote Time
-		if (IsOnFloor())
-		{
-			coyoteTimer.Reset();
-			canJump = true;
-		}
-		else if (canJump && coyoteTimer.Elapsed != TimeSpan.Zero && coyoteTimer.Elapsed < coyoteTime)
-		{
-			coyoteTimer.Start();
-			canJump = true;
-		}
-		else
-		{
-			coyoteTimer.Stop();
-			canJump = false;
-		}
-
-		// Implement Jump Buffering
-		if (Input.IsActionPressed("jump"))
-		{
-			Jump();
-		}
-
-		MoveTowards(direction);
-	}
-
-	private void MoveTowards(Vector3 direction)
-	{
-		// Apply slowdown when the player stops moving
-		if (direction == Vector3.Zero)
-		{
-			_velocity.X = Mathf.Lerp(_velocity.X, 0f, 0.1f);
-			_velocity.Z = Mathf.Lerp(_velocity.Z, 0f, 0.1f);
-		}
-		else
-		{
-			_velocity.X = direction.X * Speed;
-			_velocity.Z = direction.Z * Speed;
-		}
-	}
-
-	private void Jump()
-	{
-		if (canJump)
-		{
-			_velocity.Y = JumpImpulse;
-			canJump = false;
-			coyoteTimer.Reset();
-		}
-	}
-
-	private void Move()
-	{
-		Velocity = _velocity;
-        GD.Print("Velocity: " + Velocity);
+    private void Move()
+    {
+        Velocity = _velocity;
+        //GD.Print("Velocity: " + Velocity);
         MoveAndSlide();
-	}
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		HandleInput(delta);
-		ApplyGravity(delta);
-		Move();
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        HandleInput(delta);
+        ApplyGravity(delta);
+        Move();
+    }
 
-	#endregion Methods
+    #endregion Methods
 
-	// Method for throwing object
-	// Object needs to be thrown in an arc to a location.
+    // Method for throwing object
+    // Object needs to be thrown in an arc to a location.
 
-	// Components needed:
-	// Health
-	// Movespeed
+    // Components needed:
+    // Health
+    // Movespeed
 
-	// Class Friend:
-	//
+    // Class Friend:
+    //
 }
