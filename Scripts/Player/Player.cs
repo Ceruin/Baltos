55ef5a2517f84b703487f48cfd84899f920ccadd
Ceruin <13532471+Ceruin@users.noreply.github.com>
2024-01-05 01:17:48 -0500

    .

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -34,15 +34,6 @@ public partial class Player : CharacterBody3D
 
     #endregion Properties
 
-    public static class PlayerStrings
-    {
-        public static StringName move_up = new StringName("move_up");
-        public static StringName move_down = new StringName("move_down");
-        public static StringName move_left = new StringName("move_left");
-        public static StringName move_right = new StringName("move_right");
-        public static StringName jump = new StringName("jump");
-    }
-
     #region Methods
 
     private void ApplyGravity(double delta)
@@ -209,14 +200,4 @@ public partial class Player : CharacterBody3D
     }
 
     #endregion Methods
-
-    // Method for throwing object
-    // Object needs to be thrown in an arc to a location.
-
-    // Components needed:
-    // Health
-    // Movespeed
-
-    // Class Friend:
-    //
 }
---
+++ Scripts/Player/PlayerStrings.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class PlayerStrings
+{
+    public static StringName move_up = new StringName("move_up");
+    public static StringName move_down = new StringName("move_down");
+    public static StringName move_left = new StringName("move_left");
+    public static StringName move_right = new StringName("move_right");
+    public static StringName jump = new StringName("jump");
+}
